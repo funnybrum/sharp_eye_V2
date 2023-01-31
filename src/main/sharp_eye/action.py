ebcfb5583c9c4ec154e14208a32a2f13d2b64e87
brum <funnybrum@gmail.com>
2023-01-31 21:01:54 +0200

    A bit more logging

--- src/main/sharp_eye/action.py
+++ src/main/sharp_eye/action.py
@@ -2,6 +2,7 @@ from io import BytesIO
 import cv2
 
 from lib import config
+from lib.log import log
 from lib.notifier_client import send_notification
 
 
@@ -11,6 +12,7 @@ def alert(frame):
     """
     thresholds = config['notifications']['threshold']
     if thresholds['pixels'] > frame.non_zero_pixels or thresholds['percent'] > frame.non_zero_percent:
+        log("Skipping notification for %s" % frame.get_metadata())
         return
 
     image = BytesIO(cv2.imencode('.jpg', cv2.resize(frame.get_motion_frame(), (0, 0), fx=0.25, fy=0.25))[1].tostring())
