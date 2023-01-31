c5cd76d9677a23a6873d624bb3f8ad80965812ef
brum <funnybrum@gmail.com>
2023-01-31 20:45:25 +0200

    Adding motion image to the notification

--- src/main/lib/notifier_client.py
+++ src/main/lib/notifier_client.py
@@ -1,16 +1,25 @@
 import requests
+import uuid
 
 from . import config
 
 
-def send_notification(message):
+def send_notification(message, image):
+    service_url = config['notifier']['url']
+    auth_token = config['notifier']['token']
+    image_name = "%s.jpg" % str(uuid.uuid4())
+
+    upload_url = "%s/image?auth_token=%s" % (service_url, auth_token)
+    requests.put(upload_url, files={image_name: image})
+
     payload = {
         'event': "camera_motion",
         'message': {
-            'text': message
+            'text': message,
+            'image': image_name,
+            'thumbnail': True
         }
     }
-    cookies = {
-        'auth_token': config['notifier']['token']
-    }
-    requests.post(config['notifier']['url'], json=payload, cookies=cookies)
+
+    push_url = "%s/push?auth_token=%s" % (service_url, auth_token)
+    requests.post(push_url, json=payload)
--- src/main/sharp_eye/action.py
+++ src/main/sharp_eye/action.py
@@ -9,17 +9,17 @@ def alert(frame):
     """
     Callback for sending an alert.
     """
-    mf = {
-        'data': BytesIO(cv2.imencode('.jpg', cv2.resize(frame.get_motion_frame(), (0, 0), fx=0.25, fy=0.25))[1].tostring()),
-        'name': 'motion.jpg'}
+    # mf = {
+    #     'data': BytesIO(cv2.imencode('.jpg', cv2.resize(frame.get_motion_frame(), (0, 0), fx=0.25, fy=0.25))[1].tostring()),
+    #     'name': 'motion.jpg'}
     f = {
         'data': BytesIO(cv2.imencode('.jpg', cv2.resize(frame.frame, (0, 0), fx=0.5, fy=0.5))[1].tostring()),
         'name': 'snapshot.jpg'}
-    pf = {
-        'data': BytesIO(cv2.imencode('.jpg', cv2.resize(frame.prev_frame, (0, 0), fx=0.5, fy=0.5))[1].tostring()),
-        'name': 'prev_snapshot.jpg'}
+    # pf = {
+    #     'data': BytesIO(cv2.imencode('.jpg', cv2.resize(frame.prev_frame, (0, 0), fx=0.5, fy=0.5))[1].tostring()),
+    #     'name': 'prev_snapshot.jpg'}
 
     text = 'Got motion on %s' % config['identifier'] + '\n' + str(frame.get_metadata())
 
-    send_notification(text)
+    send_notification(text, f)
     # send_email(subject, text, [mf, f, pf])
