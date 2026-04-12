167502d2784f36fcdeababcb6f6a40ddacfa3433
brum <funnybrum@gmail.com>
2026-04-13 00:19:33 +0300

    Another attempt to fix the warninigs

--- src/main/object_detector.sh
+++ src/main/object_detector.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 cd /sharp_eye
-APP_CONFIG=./resources/object_detector.yaml python3 object_detector.py
+APP_CONFIG=./resources/object_detector.yaml YOLO_CONFIG_DIR=/tmp/Ultralytics python3 object_detector.py
